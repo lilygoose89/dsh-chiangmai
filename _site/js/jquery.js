44a1996f4b7b609379d82a5d6d857c3b092d80be
Gus <gustavo.schiavon@gmail.com>
2020-12-27 17:17:32 -0300

    Menu functionality

--- _site/js/jquery.js
+++ _site/js/jquery.js
@@ -1,19 +1,14 @@
 
 $(document).ready(function(){
- console.log('jQuery loaded')
-
-  $('.burger').click(function(){
+  $('#burger').click(function(){
     $(this).toggleClass('open');
-
-    $('#mobile-menu').toggleClass('hide');
-
-  console.log('Burger clicked')
+    $('#nav-menu').toggleClass('hide');
   });
 
-  $('.mobile-menu a').click(function(){
+  $('.nav-menu a').click(function(){
     $('#mobile-menu').toggleClass('hide');
 
-    $('.burger').toggleClass('open');
+    $('#burger').toggleClass('open');
 
   });
   $(window).on('scroll', function () {
